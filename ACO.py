106f2302c5700096a99ad04313b7e03471b51d85
William Vukasovic <wjbodyv@gmail.com>
2018-11-03 19:03:47 -0500

    Added functions to generate city and trail matrix

--- ACO.py
+++ ACO.py
@@ -1,1 +1,28 @@
-print("Pushing to test branch")
+import random as rand
+
+def generate_matrix(no_cities):
+    cities = []
+    for i in range(no_cities):
+        row = []
+        for j in range(no_cities):
+            row.append(rand.randint(0, 1))
+        cities.append(row)
+    return cities
+
+def generate_trails(city_matrix):
+    city_count = len(city_matrix)
+    trails = []
+    for i in range(city_count):
+        row = []
+        for j in range(city_count):
+            if(city_matrix[i][j] == 1):
+                row.append(rand.randint(0, 100))
+            else:
+                row.append(0)
+        trails.append(row)
+    return trails
+
+city_matrix = generate_matrix(10)
+trail_matrix = generate_trails(city_matrix)
+print(city_matrix)
+print(trail_matrix)

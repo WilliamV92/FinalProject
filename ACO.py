8e178b09886ef2d42aef08da2e220a83afa4cb70
William Vukasovic <wjbodyv@gmail.com>
2018-11-04 19:10:16 -0600

    Added code for probability calculation and choosing next city

--- ACO.py
+++ ACO.py
@@ -62,7 +62,7 @@ class Ant:
         current_city_id = start_city_id
         for i in range(0, num_cities):
             # determine probability to travel to each city from current city
-            visit_probailities = self.calculate_probailities(current_city_id)
+            visit_probailities = self.calculate_probabilities(current_city_id)
             # choose next city to visit
             next_city_id = self.choose_next_city(visit_probailities)
             # visit city and update trail length
@@ -71,32 +71,38 @@ class Ant:
             current_city_id = next_city_id
         # when I have visited final city, go back to start, and update tour length?
     
-    def calculate_probailities(self, current_city_id):
+    def calculate_probabilities(self, current_city_id):
         # at index 0 is the probability for visiting city with id 0
         visit_probabilities = []
-        # calculate sum of (T(i-j) * alpha) * (N(i-j) * beta) for all cities ant has not yet visited
-        #       loop through all cities, check if visited, if not, add to sigma value
-        # calculate probailities for all cities any has not visited yet:
-        # pheromone_trails (T(i-j) * alpha) * (N(i-j) * beta) / sigma
-        #       loop throug all cities, if visited probaility = 0, else calculate probaility
+        trail_contributions = []
+        pheromone_total = 0.0
+        for i in range(self.problem.num_cities):
+            if i not in self.visited_cities and i != current_city_id:
+                edge_pheromone = math.pow(self.problem.pheromone_trails[current_city_id][i], self.problem.alpha)
+                edge_cost = math.pow(1.0 / self.problem.distance_matrix[current_city_id][i], self.problem.beta)
+                # this calculation is used below as well
+                # storing it in an array for later use
+                trail_contributions.append(edge_pheromone * edge_cost)
+                pheromone_total += trail_contributions[i]
+            else:
+                trail_contributions.append(0)
+
+        for i in range(self.problem.num_cities):
+            if(i in self.visited_cities):
+                visit_probabilities.append(0)
+            else:
+                visit_probabilities.append(trail_contributions[i] / pheromone_total)
         return visit_probabilities
     
     def choose_next_city(self, visit_probabilities):
         # make choice of which city to visit based on probailities
-        city_id = 0
-        return city_id
-
-
-# ant:
-    # adds to know: (1) the map; (2) matrix of pheromenes (pheromone_trails[][])
-    # list of visited cities: A, D, E, F
-    # function to mark city as visited
-    #    def visit_city(city_id):
-    #        visited_cities.add(city_id)
-    # calculate tour length (based on visitied cities and map)
-    # do_tour() generate_solution()
-    #   pick next city()
-    #       calculate probabilities()
+        accumulated_probability = 0.0
+        rand_value = rand.uniform(0, 1)
+        for i in range(len(visit_probabilities)):
+            accumulated_probability += visit_probabilities[i]
+            if accumulated_probability > rand_value:
+                return i
+        return len(visit_probabilities) - 1
 
 
 class AntColony:
@@ -134,12 +140,15 @@ class AntColony:
 
 class Problem:
     def __init__(self, num_cities):
+        self.alpha = 1
+        self.beta = 5
         self.map = Map(num_cities)
+        self.num_cities = num_cities
         self.distance_matrix = []
         self.pheromone_trails = []
-        self.initialize(num_cities)
+        self.initialize()
     
-    def initialize(self, num_cities):
+    def initialize(self):
         # pre-calculate distances between all cities in maps
         self.distance_matrix = [] 
         for i in range(len(self.map.cities.keys())):
@@ -157,9 +166,9 @@ class Problem:
             self.distance_matrix.append(temp_distances)
         # prepopulate pheromone matrix with value of 1 for each edge
         self.pheromone_trails = []
-        for i in range(num_cities):
+        for i in range(self.num_cities):
             temp_trail = []
-            for j in range(num_cities):
+            for j in range(self.num_cities):
                 temp_trail.append(1)
             self.pheromone_trails.append(temp_trail)
 
@@ -172,7 +181,10 @@ def main():
     colony.solve_problem()
 
 problem = Problem(10)
-for x in range(len(problem.distance_matrix)):
-    for y in range(len(problem.distance_matrix)):
-        print(problem.distance_matrix[x][y], end=",", flush=True)
-    print()
+ant = Ant(problem)
+print(ant.choose_next_city(ant.calculate_probabilities(0)))
+# problem = Problem(10)
+# for x in range(len(problem.distance_matrix)):
+#     for y in range(len(problem.distance_matrix)):
+#         print(problem.distance_matrix[x][y], end=",", flush=True)
+#     print()
